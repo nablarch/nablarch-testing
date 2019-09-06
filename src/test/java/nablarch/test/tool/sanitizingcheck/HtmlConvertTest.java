a19e1d24b91d825097bede3c760dbb77a80bbb49
seko.masaya <seko.masaya@tis.co.jp>
2019-09-06 18:43:23 +0900

    Java6とJava8で出力結果が異なる部分について、テスト結果を場合分けした。

--- src/test/java/nablarch/test/tool/sanitizingcheck/HtmlConvertTest.java
+++ src/test/java/nablarch/test/tool/sanitizingcheck/HtmlConvertTest.java
@@ -1,12 +1,17 @@
 package nablarch.test.tool.sanitizingcheck;
 
 
+import org.junit.Assume;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.TemporaryFolder;
 
 import java.io.IOException;
+import java.math.BigDecimal;
 
+import static org.hamcrest.CoreMatchers.not;
+import static org.hamcrest.Matchers.lessThanOrEqualTo;
+import static org.hamcrest.core.AllOf.allOf;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
 
@@ -24,7 +29,7 @@ public class HtmlConvertTest extends SanitizingCheckTestSupport {
      * mainメソッドテスト
      */
     @Test
-    public void testMain() throws IOException {
+    public void testMainErrorCase() throws IOException {
 
         // 引数が2の場合
         String[] args = new String[2];
@@ -36,14 +41,6 @@ public class HtmlConvertTest extends SanitizingCheckTestSupport {
         } catch (IllegalArgumentException e) {
             assertEquals("enter paths of xml, xslt and html.", e.getMessage());
         }
-        
-        // 引数が3の場合
-        args = new String[3];
-        args[0] = "src/test/java/nablarch/test/tool/sanitizingcheck/testHtmlConvert.xml";
-        args[1] = "src/test/java/nablarch/test/tool/sanitizingcheck/TransformToHTML.xsl";
-        args[2] = tmpDir.newFile().getAbsolutePath();
-        HtmlConvert.main(args);
-        assertFile("src/test/java/nablarch/test/tool/sanitizingcheck/expected/expectedHtml.html", args[2]);
         
         // 引数が4の場合
         args = new String[4];
@@ -82,4 +79,43 @@ public class HtmlConvertTest extends SanitizingCheckTestSupport {
             assertEquals("Check result file doesn't exist.", e.getMessage());
         }
     }
+
+    /**
+     * 正常系のテスト(Java6以前用)。
+     *
+     * 出力されされるHTMLが変換結果が、jdk1.8_151以前と以後で異なるため場合わけしている。<br />
+     * テスト環境で、jdk1.8_151以前に該当するのはJava6の環境なので、この場合分けとしている。
+     * @throws IOException
+     */
+    @Test
+    public void testMainSuccessCaseBeforeJava6() throws IOException {
+        Assume.assumeTrue(new BigDecimal(System.getProperty("java.specification.version")).compareTo(new BigDecimal("1.6")) <= 0 );
+
+        // 引数が3かつ、引数が適切な場合。
+        String[] args = new String[3];
+        args[0] = "src/test/java/nablarch/test/tool/sanitizingcheck/testHtmlConvert.xml";
+        args[1] = "src/test/java/nablarch/test/tool/sanitizingcheck/TransformToHTML.xsl";
+        args[2] = tmpDir.newFile().getAbsolutePath();
+        HtmlConvert.main(args);
+        assertFile("src/test/java/nablarch/test/tool/sanitizingcheck/expected/expectedHtmlBeforeJava6.html", args[2]);
+    }
+
+    /**
+     * 正常系のテスト(Java8以降用)。
+     *
+     * @throws IOException
+     */
+    @Test
+    public void testMainSuccessCasAfterJava8() throws IOException {
+        Assume.assumeTrue(new BigDecimal(System.getProperty("java.specification.version")).compareTo(new BigDecimal("1.8")) >= 0 );
+
+        // 引数が3かつ、引数が適切な場合。
+        String[] args = new String[3];
+        args[0] = "src/test/java/nablarch/test/tool/sanitizingcheck/testHtmlConvert.xml";
+        args[1] = "src/test/java/nablarch/test/tool/sanitizingcheck/TransformToHTML.xsl";
+        args[2] = tmpDir.newFile().getAbsolutePath();
+        HtmlConvert.main(args);
+        assertFile("src/test/java/nablarch/test/tool/sanitizingcheck/expected/expectedHtmlAfterJava8.html", args[2]);
+    }
+
 }
